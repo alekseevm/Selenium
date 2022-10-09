d930a7aaabbb1b477aca26b125893349081f26e7
alekseev <alekseev.m@me.com>
2022-10-09 23:00:16 +0300

    update2

--- src/test/java/OrderCardTest.java
+++ src/test/java/OrderCardTest.java
@@ -2,6 +2,7 @@
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
         import static com.codeborne.selenide.Condition.exactText;
         import static com.codeborne.selenide.Selenide.$;
         import static com.codeborne.selenide.Selenide.open;
