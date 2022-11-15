a517f7f51ccdbe6730c88c941cfab18e3fd6cd5c
alekseev <alekseev.m@me.com>
2022-11-15 18:46:42 +0300

    update4

--- src/test/java/OrderCardTest.java
+++ src/test/java/OrderCardTest.java
@@ -1,4 +1,3 @@
-
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -8,19 +7,17 @@ import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-
 public class OrderCardTest {
 
     @BeforeEach
-    void shouldOpenUrlInBrowser() {
+    void openUrlInBrowser() {
         open("http://localhost:9999");
     }
 
-
     @Test
-    void shouldOrderFormCorrect() {
+    void shouldSubmitRequest() {
         SelenideElement form = $("form");
-        form.$("[data-test-id=name] input").setValue("Иван Николаев-Иванов");
+        form.$("[data-test-id=name] input").setValue("Василий Иванов");
         form.$("[data-test-id=phone] input").setValue("+79991234567");
         form.$("[data-test-id=agreement]").click();
         form.$(".button").click();
@@ -28,46 +25,51 @@ public class OrderCardTest {
     }
 
     @Test
-    void shouldIncorrectNameField() {
+    void shouldValidateNameField() {
         SelenideElement form = $("form");
-        form.$("[data-test-id=name] input").setValue("11");
+        form.$("[data-test-id=name] input").setValue("1");
+        form.$("[data-test-id=phone] input").setValue("+79991234567");
+        form.$("[data-test-id=agreement]").click();
         form.$(".button").click();
         $("[data-test-id='name'].input_invalid .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
 
     @Test
-    void shouldIncorrectPhoneField() {
+    void nameFieldCouldNotBeEmpty() {
         SelenideElement form = $("form");
-        form.$("[data-test-id=name] input").setValue("Иван Николаев-Иванов");
-        form.$("[data-test-id=phone] input").setValue("+7999");
-        form.$(".button").click();
-        $("[data-test-id=phone].input_invalid .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
-    }
-
-    @Test
-    void shouldIfNameFieldEmpty() {
-        SelenideElement form = $("form");
+        form.$("[data-test-id=name] input").setValue("");
         form.$("[data-test-id=phone] input").setValue("+79991234567");
+        form.$("[data-test-id=agreement]").click();
         form.$(".button").click();
         $("[data-test-id='name'].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
     }
 
     @Test
-    void shouldIfPhoneFieldEmpty() {
+    void shouldValidatePhoneField() {
         SelenideElement form = $("form");
-        form.$("[data-test-id=name] input").setValue("Иван Николаев-Иванов");
+        form.$("[data-test-id=name] input").setValue("Василий Иванов");
+        form.$("[data-test-id=phone] input").setValue("+");
+        form.$("[data-test-id=agreement]").click();
+        form.$(".button").click();
+        $("[data-test-id=phone].input_invalid .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+
+    @Test
+    void phoneFieldCouldNotBeEmpty() {
+        SelenideElement form = $("form");
+        form.$("[data-test-id=name] input").setValue("Василий Иванов");
+        form.$("[data-test-id=phone] input").setValue("");
+        form.$("[data-test-id=agreement]").click();
         form.$(".button").click();
         $("[data-test-id=phone].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
     }
 
     @Test
-    void shouldCheckboxNotMarked() {
-        open("http://localhost:9999");
+    void checkboxTextShouldBeRedIfNotMarked() {
         SelenideElement form = $("form");
-        form.$("[data-test-id=name] input").setValue("Иван Николаев-Иванов");
+        form.$("[data-test-id=name] input").setValue("Василий Иванов");
         form.$("[data-test-id=phone] input").setValue("+79991234567");
         form.$(".button").click();
         $("[data-test-id=agreement].input_invalid").should(visible);
-
     }
 }
